73c85be89b6ac755a1796817338f484cb58073b3
Yohann Dumortier <yohann.dumortier@gmail.com>
2018-06-11 13:46:12 +0200

    modication des option de connection

--- QT/projet/moc_mainwindow.cpp
+++ QT/projet/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../QT/projet/mainwindow.h"
+#include "mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[7];
-    char stringdata0[117];
+    QByteArrayData data[13];
+    char stringdata0[175];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -35,12 +35,20 @@ QT_MOC_LITERAL(2, 34, 0), // ""
 QT_MOC_LITERAL(3, 35, 21), // "on_pushButton_clicked"
 QT_MOC_LITERAL(4, 57, 18), // "on_Annuler_clicked"
 QT_MOC_LITERAL(5, 76, 20), // "on_Confirmer_clicked"
-QT_MOC_LITERAL(6, 97, 19) // "on_question_clicked"
+QT_MOC_LITERAL(6, 97, 19), // "on_question_clicked"
+QT_MOC_LITERAL(7, 117, 7), // "AddRoot"
+QT_MOC_LITERAL(8, 125, 4), // "name"
+QT_MOC_LITERAL(9, 130, 11), // "Description"
+QT_MOC_LITERAL(10, 142, 8), // "Addchild"
+QT_MOC_LITERAL(11, 151, 16), // "QTreeWidgetItem*"
+QT_MOC_LITERAL(12, 168, 6) // "parent"
 
     },
     "MainWindow\0on_Deconnexion_clicked\0\0"
     "on_pushButton_clicked\0on_Annuler_clicked\0"
-    "on_Confirmer_clicked\0on_question_clicked"
+    "on_Confirmer_clicked\0on_question_clicked\0"
+    "AddRoot\0name\0Description\0Addchild\0"
+    "QTreeWidgetItem*\0parent"
 };
 #undef QT_MOC_LITERAL
 
@@ -50,7 +58,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -58,11 +66,13 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   39,    2, 0x08 /* Private */,
-       3,    0,   40,    2, 0x08 /* Private */,
-       4,    0,   41,    2, 0x08 /* Private */,
-       5,    0,   42,    2, 0x08 /* Private */,
-       6,    0,   43,    2, 0x08 /* Private */,
+       1,    0,   49,    2, 0x08 /* Private */,
+       3,    0,   50,    2, 0x08 /* Private */,
+       4,    0,   51,    2, 0x08 /* Private */,
+       5,    0,   52,    2, 0x08 /* Private */,
+       6,    0,   53,    2, 0x08 /* Private */,
+       7,    2,   54,    2, 0x08 /* Private */,
+      10,    3,   59,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -70,6 +80,8 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,    8,    9,
+    QMetaType::Void, 0x80000000 | 11, QMetaType::QString, QMetaType::QString,   12,    8,    9,
 
        0        // eod
 };
@@ -85,10 +97,11 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 2: _t->on_Annuler_clicked(); break;
         case 3: _t->on_Confirmer_clicked(); break;
         case 4: _t->on_question_clicked(); break;
+        case 5: _t->AddRoot((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        case 6: _t->Addchild((*reinterpret_cast< QTreeWidgetItem*(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2])),(*reinterpret_cast< QString(*)>(_a[3]))); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
 const QMetaObject MainWindow::staticMetaObject = {
@@ -116,13 +129,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 7)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 7;
     }
     return _id;
 }
